e5e962e4df715b8690cc25e6ef6e793db417ee0f
marutjames <jamesmarutjaipala@gmail.com>
2020-02-26 18:22:36 +0700

    start

---
+++ FlatLib/AllskyPlotGraph.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
+using Emgu.CV.CvEnum;
+
+namespace FlatLib
+{
+   public class AllskyPlotGraph
+    {
+        private Image<Bgr, byte> allSkyImage;
+        private double azm;
+        private double alt;
+        private int telescopeRadius = 5;
+
+        public Image<Bgr, byte> plotTelescope(Image<Bgr, byte> allSkyImage, Point point, int telescopeRadius = 5)
+        {
+            this.allSkyImage = allSkyImage;
+
+            this.telescopeRadius = telescopeRadius;
+
+            Image<Bgr, byte> allskyplot = addGraph(allSkyImage);
+
+
+            Point pointTextAzm = point;
+            pointTextAzm.X = pointTextAzm.X + 20;
+            Point pointTextAlt = pointTextAzm;
+            pointTextAlt.Y = pointTextAlt.Y + 25;
+
+            CvInvoke.Circle(allskyplot, point, telescopeRadius, new MCvScalar(0, 0, 255), 2);
+            CvInvoke.PutText(allskyplot, "Azm: " + azm, pointTextAzm, FontFace.HersheySimplex, 0.65, new Bgr(Color.Red).MCvScalar);
+            CvInvoke.PutText(allskyplot, "Alt: " + alt, pointTextAlt, FontFace.HersheySimplex, 0.65, new Bgr(Color.Red).MCvScalar);
+
+            return (allskyplot);
+        }
+        public Point calculatePoint(Double azm, Double alt)
+        {
+            this.alt = alt;
+            this.azm = azm;
+            int X, Y;
+            int A = (int)(500 / 2.0), B = (int)(500 / 2.0);
+            azm = azm * Math.PI / 180.0;
+            alt = alt * Math.PI / 180.0;
+            azm = azm - (Math.PI / 2.0);
+            Double R = calculateRadius(alt);
+            //R = R * (-1);
+            X = Convert.ToInt32(A + (R * -1) * Math.Cos(azm));
+            Y = Convert.ToInt32(B + R * Math.Sin(azm));
+            Point point = new Point(X - Convert.ToInt32(telescopeRadius), Y - Convert.ToInt32(telescopeRadius));
+            return (point);
+        }
+
+
+        private Double calculateRadius(Double El)
+        {
+            Double Radius = 0;
+            if (El > 0)
+                try
+                {
+                    Radius = El * (500 / 4.0) / (Double)(Math.PI / 4.0);
+                    Radius = 500 / 2.0 - Radius;
+                }
+                catch { }
+            else
+                Radius = 500 / 2.0;
+            return Radius;
+        }
+
+
+        private Image<Bgr, byte> addGraph(Image<Bgr, byte> allSkyImage)
+        {
+            Image<Bgr, byte> imgBg = new Image<Bgr, byte>(640, 640);
+            Image<Bgr, byte> addImgBg = imgBg;
+            int X1 = (imgBg.Height - allSkyImage.Height) / 2; // px bg0 -> img
+            int X2 = X1 + allSkyImage.Height; // px img -> bg640
+            for (int i = 0; i < imgBg.Height; i++)
+            {
+                for (int j = 0; j < imgBg.Width; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        if (i <= X1 || i >= X2)
+                        {
+                            addImgBg.Data[i, j, k] = 255;
+                        }
+                        else
+                        {
+                            addImgBg.Data[i, j, k] = this.allSkyImage.Data[i - X1, j, k];
+                        }
+                    }
+                }
+            }
+            CvInvoke.Circle(addImgBg, new Point((addImgBg.Height / 2), (addImgBg.Width / 2)), 320, new MCvScalar(0, 0, 0), 2);
+            return (addImgBg);
+        }
+    }
+}
---
+++ FlatLib/ExposureCal.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FlatLib
+{
+    public class ExposuerCal
+    {
+ 
+            private string filterName; // Filter Name
+            private double lastAdu; // Lasted Adu 
+            private double lastExposureTime; // Lasted Exposure Time
+            private double targetAdu; // Adu Target 
+            private int grabTime; // grab at time  ** start at 1 **
+            private double exposureTimeCalculate; // calculated exposure Time  for grab 
+            private double variableAdu; // use for calculate next grab
+            private static MongoClient client = new MongoClient();
+
+            public string databaseName = "CCDFlat";
+
+
+
+
+            public double flatExposureTime(string filterName, double lastAdu, double lastExposureTime, double targetAdu, int grabTime)
+            {
+                this.filterName = filterName;
+                this.lastAdu = lastAdu;
+                this.lastExposureTime = lastExposureTime;
+                this.targetAdu = targetAdu;
+                this.grabTime = grabTime;
+
+                if (this.grabTime == 1) // frist grab for use frist Adu referent  .....
+                {
+                    this.variableAdu = this.lastAdu;
+                }
+                double aduCostError = this.lastAdu - this.targetAdu; // find difference between lastAdu and adu Target ..
+
+                if (aduCostError < 0) // LastADU less than TargetADU..
+                {
+                    this.variableAdu = this.variableAdu + Math.Abs(aduCostError);
+                }
+                else if (aduCostError > 0) // LastADU more than TargetADU..
+                {
+                    this.variableAdu = variableAdu - Math.Abs(aduCostError);
+                }
+                exposureTimeCalculate = exposureCal(this.lastExposureTime, this.lastAdu, variableAdu); //Calculate the next exposure Time ..
+
+                return (this.exposureTimeCalculate);
+            }
+
+            private double exposureCal(double oldExposure, double adu, double variableAdu)
+            {
+                double simulete = adu / oldExposure;
+                double exposureCal = variableAdu / simulete;
+                return (exposureCal);
+            }
+
+            public double exposureTimeFristGrab(double sunAlt, string filter, double targetAdu)
+            {
+                double fristExposureTime = 0;
+                try
+                {
+                    IMongoDatabase db = client.GetDatabase(this.databaseName);
+                    var things = db.GetCollection<CCD_Mongo>("data");
+                    var sunaltDB = things.AsQueryable().Where(x => x.SunAltStart >= (sunAlt - 0.2) & x.SunAltStart < (sunAlt + 0.2) & x.Filter == filter & x.Adu >= (targetAdu - 1000) & x.Adu <= (targetAdu + 1000)).Max(x => x.SunAltStart);
+                    var command = things.AsQueryable().Where(y => y.SunAltStart == sunaltDB & y.Filter == filter).FirstOrDefault();
+                    fristExposureTime = command.ExposureTime;
+                }
+                catch
+                {
+                    fristExposureTime = 1.00;
+                }
+                return (fristExposureTime);
+
+            }
+        }
+    }
+
+
+internal class CCD_Mongo
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    [BsonElement("SunAltStart")]
+    public double SunAltStart { get; set; }
+
+    [BsonElement("SunAltEnd")]
+    public double SunAltEnd { get; set; }
+
+    [BsonElement("SunAzmStart")]
+    public double SunAzmStart { get; set; }
+    [BsonElement("SunAzmEnd")]
+    public double SunAzmEnd { get; set; }
+
+    [BsonElement("Filter")]
+    public string Filter { get; set; }
+
+    [BsonElement("ExposureTime")]
+    public double ExposureTime { get; set; }
+
+    [BsonElement("Adu")]
+    public double Adu { get; set; }
+
+    [BsonElement("FitsImage")]
+    public string FitsImage { get; set; }
+
+    [BsonElement("JpgImage")]
+    public string JpgImage { get; set; }
+
+    [BsonElement("AllskyImage")]
+    public string AllskyImage { get; set; }
+
+    [BsonElement("DateTime")]
+    public DateTime DateTime { get; set; }
+}
+
